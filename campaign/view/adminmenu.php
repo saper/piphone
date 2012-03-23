479f536d90d4559f8ff0c2ada27692d4e80a7ad1
Benjamin Sonntag <benjamin@sonntag.fr>
2012-03-23 23:14:31 +0100

    adding missing admin menu

---
+++ campaign/view/adminmenu.php
@@ -0,0 +1,12 @@
+
+<?php require_once("head.php"); ?>
+<h2><?php __("Piphone Administration"); ?></h2>
+
+<?php show_messages(); ?>
+
+<ul>
+<li><a href="/user"><?php __("Manage users"); ?></li>
+<li><a href="/campaign"><?php __("Manage campaigns"); ?></li>
+</ul>
+<?php require_once("foot.php"); ?>
+
