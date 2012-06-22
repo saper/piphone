2399ac862cf6d9d6214b56903ae6f58b9e876b80
Benjamin Sonntag <benjamin@sonntag.fr>
2012-06-22 22:10:33 +0200

    adding country  + style on list editor

--- campaign/view/adminlistcallee.php
+++ campaign/view/adminlistcallee.php
@@ -11,6 +11,7 @@
   <th>Enabled</th>
   <th>Disabled</th>
   <th>Name</th>
+  <th>Country</th>
   <th>Number</th>
   <th>CallCount</th>
 </tr>
@@ -19,6 +20,7 @@
 <td><input type="radio" name="callee[<?php echo $callee["id"]; ?>]" value="1" id="callee<?php echo $callee["id"]; ?>_1"<?php checked($callee["enabled"]); ?> /><label class="radio" for="callee<?php echo $callee["id"]; ?>_1">Enabled</label></td>
 <td><input type="radio" name="callee[<?php echo $callee["id"]; ?>]" value="0" id="callee<?php echo $callee["id"]; ?>_0"<?php checked(!$callee["enabled"]); ?> /><label class="radio" for="callee<?php echo $callee["id"]; ?>_0">Disable</label></td>
    <td><?php echo $callee["name"]; ?></td>
+   <td><?php echo $callee["country"]; ?></td>
    <td><?php echo $callee["phone"]; ?></td>
    <td><?php echo $callee["callcount"]; ?></td>
 </tr>
