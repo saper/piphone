790ec0fb0b883601e4f118b983aee45e1faa05c1
Benjamin Sonntag <benjamin@sonntag.fr>
2012-06-25 10:30:07 +0200

    new dump + getliste is now a nice php script to import data from memopol + metadata also ...

--- misc/dump.sh
+++ misc/dump.sh
@@ -5,14 +5,31 @@
 
 # USAGE: change the mysql user/pass at the bottom and also the campaign=2 by your campaign-id
 
+# BRUXELLES : 
+#(
+#    echo "SET NAMES UTF8;" 
+#    wget --no-check-certificate -q -O - https://memopol.lqdn.fr/europe/parliament/names |
+#    egrep "(/europe/parliament/deputy/|callto://\+33|/europe/parliament/country/|/europe/parliament/group/)" |
+#    grep -v "More information" | 
+#    grep -v "By country" | 
+#    grep -v "By political group" | 
+#    sed -e 's/^.*"\/europe\/parliament\/deputy\/\([^"]*\)">\([^<]*\)<.*$/INSERT INTO lists SET campaign=14, name="\2", url="https:\/\/memopol.lqdn.fr\/europe\/parliament\/deputy\/\1";/' \
+#	-e 's/^.*callto:\/\/.\([0-9]*\).*callto:\/\/.\([0-9]*\).*$/UPDATE lists SET phone="00\1" WHERE phone="";/' \
+#	-e 's/^.*country\/\(..\)\/.*$/UPDATE lists SET country="\1" WHERE country="";/' \
+#	-e 's/^.*\/group\/\([^"]*\)">.*$/UPDATE lists SET name=CONCAT(name," (\1)"), callduration=1 WHERE callduration=0;/'
+#) | mysql piphone # if necesssary : -u<user> -p<password>
+
+
+# Strasbourg
+
 (
     echo "SET NAMES UTF8;" 
-    wget -q -O - https://memopol.lqdn.fr/europe/parliament/names |
-    egrep "(/europe/parliament/deputy/|callto://\+33|/europe/parliament/country/|/europe/parliament/group/)" |
+    wget --no-check-certificate -q -O - https://memopol.lqdn.fr/europe/parliament/names |
+    egrep "(/europe/parliament/deputy/|callto://\+32|/europe/parliament/country/|/europe/parliament/group/)" |
     grep -v "More information" | 
     grep -v "By country" | 
     grep -v "By political group" | 
-    sed -e 's/^.*"\/europe\/parliament\/deputy\/\([^"]*\)">\([^<]*\)<.*$/INSERT INTO lists SET campaign=2, name="\2", url="https:\/\/memopol.lqdn.fr\/europe\/parliament\/deputy\/\1";/' \
+    sed -e 's/^.*"\/europe\/parliament\/deputy\/\([^"]*\)">\([^<]*\)<.*$/INSERT INTO lists SET campaign=14, name="\2", url="https:\/\/memopol.lqdn.fr\/europe\/parliament\/deputy\/\1";/' \
 	-e 's/^.*callto:\/\/.\([0-9]*\).*callto:\/\/.\([0-9]*\).*$/UPDATE lists SET phone="00\1" WHERE phone="";/' \
 	-e 's/^.*country\/\(..\)\/.*$/UPDATE lists SET country="\1" WHERE country="";/' \
 	-e 's/^.*\/group\/\([^"]*\)">.*$/UPDATE lists SET name=CONCAT(name," (\1)"), callduration=1 WHERE callduration=0;/'
