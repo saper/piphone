25dd26f9be5ad54f7674f87af62956d848a17aee
Benjamin Sonntag <benjamin@sonntag.fr>
2012-06-26 10:23:28 +0200

    adding okhin url

--- campaign/view/foot.php
+++ campaign/view/foot.php
@@ -5,7 +5,7 @@
 <!-- <div id="ajax-indicator" style="display:none;"><span>Loading...</span></div> -->
 
 <p id="footer">
-    <?php if (defined("PIPHONE_VERSION")) echo "version ".PIPHONE_VERSION; ?> &copy; La Quadrature du Net 2011-2012 <br /> code by <a href="http://www.digi-nation.com/">Digination</a>, <a href="http://benjamin.sonntag.fr/">Benjamin Sonntag</a>, Okhin, Design by <a href="http://nurpa.be/">André Loconte</a>
+    <?php if (defined("PIPHONE_VERSION")) echo "version ".PIPHONE_VERSION; ?> &copy; La Quadrature du Net 2011-2012 <br /> code by <a href="http://www.digi-nation.com/">Digination</a>, <a href="http://benjamin.sonntag.fr/">Benjamin Sonntag</a>, <a href="http://about.okhin.fr">Okhin</a>, Design by <a href="http://nurpa.be/">André Loconte</a>
 </p>
 
 </body>
