9e364f21c7e7a252ac47dc7551a81984cddd5af0
Okhin <okhin@piphone.octopuce.fr>
2012-06-29 12:16:04 +0200

    Orientation parameters, default on vert

--- campaign/controller/campaignController.php
+++ campaign/controller/campaignController.php
@@ -193,6 +193,8 @@ class campaignController extends abstractController {
     $slug=addslashes(trim($params[0]));
     $view["campaign"]=$this->_getCampaign($slug);
 
+    if (isset($params[1]) and (strcmp($params[1], "horiz") $view["orientation"]="horiz"; else $view["orientation"]="vert";
+
     // Now, we need a mep
     $view["callee"]=mqone("SELECT * FROM lists WHERE campaign='".$view["campaign"]["id"]."' AND lists.enabled=1 ORDER BY callcount ASC, RAND();");
     render("campaignwidget2");
