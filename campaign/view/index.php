cff2e60a790e0c199fa2a32a11e9b59e8d0edf70
Okhin <okhin@piphone.octopuce.fr>
2012-10-21 18:22:49 +0200

    Buttons are now next each other

--- campaign/view/index.php
+++ campaign/view/index.php
@@ -53,8 +53,8 @@ if ($c["name-".$view["lang"]]) {
 	?>
 	</p>
 	<p class="deadline"><?php echo $c["datestop"]; ?></p>
-	<p class="button"><a href="/campaign/call2/<?php echo $c["slug"]; ?>" class="blue"><?php __("Act now!"); ?></a></p>
-	<p class="button"><a href="/campaign/hof2/<?php echo $c["slug"]; ?>" class="blue"><?php __("Hall of Fame"); ?></a></p>
+	<p class="button"><a href="/campaign/call2/<?php echo $c["slug"]; ?>" class="blue"><?php __("Act now!"); ?></a>
+	<a href="/campaign/hof2/<?php echo $c["slug"]; ?>" class="blue"><?php __("Hall of Fame"); ?></a></p>
 </div>
 <?php
     }
