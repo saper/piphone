618ec39187e18507ae70c09c4e8a2ce29b691e2c
Okhin <okhin@piphone.octopuce.fr>
2013-02-06 18:13:35 +0100

    CSS with .flash (and old ones have been killed)

--- campaign/view/loginform.php
+++ campaign/view/loginform.php
@@ -9,9 +9,9 @@ You want to be in the hall of fame? Or to know who you've called until now?
 Just create an account.
 </div>
 <div class="flash error">
-<?php if (isset($view["login"])) { ?> <span><?php echo $view["login"]; ?> is not available</span> <?php } ?>
-<?php if (isset($view["email"])) { ?> <span><?php echo $view["email"]; ?> is not a valid email address</span> <?php } ?>
-<?php if (isset($view["password"])) { ?> <span>Passwords doesn't match</span> <?php } ?>
+<?php if (isset($view["login"])) { ?> <p><?php echo $view["login"]; ?> is not available</p> <?php } ?>
+<?php if (isset($view["email"])) { ?> <p><?php echo $view["email"]; ?> is not a valid email address</p> <?php } ?>
+<?php if (isset($view["password"])) { ?> <p>Passwords doesn't match</p> <?php } ?>
 </div>
 
 <div class="mep">
